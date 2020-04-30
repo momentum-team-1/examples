5249fff4f9deb21ad29f6313e3eefe2d418797a5
Amy Gori <amydgori@gmail.com>
2020-04-30 17:01:03 -0400

    confirm passwords match and show valid message

--- forms-are-fine/script.js
+++ forms-are-fine/script.js
@@ -1,11 +1,17 @@
 let form = document.querySelector('#register-form')
+let formIsValid
 
 form.addEventListener('submit', function (event) {
   event.preventDefault()
 
-  validateEmail()
-  validatePassword()
+  // Think of these two initial steps as clearing everything
+  // to give us a "clean slate" to start from
+  removeErrorMessage()
+  formIsValid = true
 
+  validateEmail()
+  confirmPasswordMatch()
+  showValidMessage()
 })
 
 function validateEmail () {
@@ -15,17 +21,44 @@ function validateEmail () {
 
   if (emailAddress) {
     //do something if it is valid
-    console.log("email address is valid")
-    parentEl.classList.remove("input-invalid")
-    parentEl.classList.add("input-valid")
+    console.log('email address is valid')
+    parentEl.classList.remove('input-invalid')
+    parentEl.classList.add('input-valid')
+    formIsValid = true
   } else {
     // do something else if it is invalid
-    console.log("email address is invalid")
-    parentEl.classList.remove("input-valid")
-    parentEl.classList.add("input-invalid")
+    console.log('email address is invalid')
+    parentEl.classList.remove('input-valid')
+    parentEl.classList.add('input-invalid')
+    formIsValid = false
   }
 }
 
-function validatePassword () {
+function confirmPasswordMatch () {
+  // grab the value of the password input
+  let password = document.querySelector('#password-input').value
+  // grab the value of the confirm password input
+  let confirmPwd = document.querySelector('#confirm-password').value
+  // compare to see if they match
 
+  if (password !== confirmPwd) {
+    // show an error message on the page
+    let errorDiv = document.querySelector('#error-msg')
+    errorDiv.innerHTML = 'Your passwords must match'
+  }
+}
+
+function removeErrorMessage () {
+  let errorDiv = document.querySelector('#error-msg')
+  errorDiv.innerHTML = ''
+}
+
+function showValidMessage () {
+  if (formIsValid) {
+    let validDiv = document.querySelector('#valid-message')
+    let validMsgEl = document.createElement('h2')
+    let validMsgText = document.createTextNode('This form is valid!')
+    validMsgEl.appendChild(validMsgText)
+    document.querySelector('main').appendChild(validMsgEl)
+  }
 }
