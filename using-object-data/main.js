c80ad6378c550063a1df2f297a3bedc09baa2838
Amy Gori <amydgori@gmail.com>
2020-05-05 15:46:17 -0400

    Add initial draft of acme menu page

---
+++ using-object-data/main.js
@@ -0,0 +1,36 @@
+/* eslint-disable prefer-const */
+
+/* DATA */
+const menuItems = [
+  {
+    title: 'Noodles',
+    imgUrl: 'https://images.unsplash.com/photo-1569718212165-3a8278d5f624?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
+  },
+  {
+    title: 'Burgers',
+    imgUrl: 'https://images.unsplash.com/photo-1550950158-d0d960dff51b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1100&q=80'
+  },
+  {
+    title: 'Breakfast',
+    imgUrl: 'https://images.unsplash.com/photo-1459789034005-ba29c5783491?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1450&q=80'
+  },
+  {
+    title: 'Dessert',
+    imgUrl: 'https://images.unsplash.com/photo-1514849302-984523450cf4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1100&q=80'
+  }
+]
+
+function renderPageContent () {
+  console.log(menuItems)
+  let menuItem = menuItems[0]
+  console.log(menuItem.imgUrl)
+
+  let targetElement = document.querySelector(".menu-items")
+  let listItemElement = document.createElement("li")
+  listItemElement.innerHTML = `<img src=${menuItem.imgUrl}>`
+  console.log(listItemElement)
+  targetElement.appendChild(listItemElement)
+}
+
+
+renderPageContent()
