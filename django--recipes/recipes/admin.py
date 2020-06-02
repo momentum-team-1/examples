cc75412c597001c057629d91df4e2165fb7ff24c
Clinton Dreisbach <clinton@dreisbach.us>
2020-06-02 17:39:06 -0400

    Work from class today

--- django--recipes/recipes/admin.py
+++ django--recipes/recipes/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Recipe, Ingredient, RecipeStep, Tag
 # Register your models here.
+
+admin.site.register(Recipe)
+admin.site.register(Ingredient)
+admin.site.register(RecipeStep)
+admin.site.register(Tag)
--- django--recipes/recipes/models.py
+++ django--recipes/recipes/models.py
@@ -1,3 +1,44 @@
 from django.db import models
+from users.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.tag
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(to=User,
+                             on_delete=models.CASCADE,
+                             related_name='recipes')
+    title = models.CharField(max_length=255)
+    prep_time_in_minutes = models.PositiveIntegerField(null=True, blank=True)
+    cook_time_in_minutes = models.PositiveIntegerField(null=True, blank=True)
+    tags = models.ManyToManyField(to=Tag, related_name="recipes")
+
+    def __str__(self):
+        return self.title
+
+
+class Ingredient(models.Model):
+    recipe = models.ForeignKey(to=Recipe,
+                               on_delete=models.CASCADE,
+                               related_name='ingredients')
+    amount = models.CharField(max_length=20)
+    item = models.CharField(max_length=255)
+
+    def __str__(self):
+        return f"{self.amount} {self.item}"
+
+
+class RecipeStep(models.Model):
+    recipe = models.ForeignKey(to=Recipe,
+                               on_delete=models.CASCADE,
+                               related_name='steps')
+    step_num = models.PositiveIntegerField()
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.step_num} {self.text}"
